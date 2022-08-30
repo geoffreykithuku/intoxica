f72c8441ff103a619e888954f8a60c20494ea588
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-08-30 20:29:14 +0300

    added more mobile responsiveness

--- index.js
+++ index.js
@@ -4,39 +4,38 @@ let searchBtn = document.getElementById('search-button');
 
 let url = 'https://thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-let userInput = document.getElementById('user-input').value;
 
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    
+let getInfo = () => {
+    let userInput = document.getElementById('user-input').value;
 
-    if (userInput == 0) {
-    result.innerHTML = `<h3 class="message">The input field cannot be empty</h3>`;
-}
-    else {
+        if (userInput == 0) {
+            result.innerHTML = `<h3 class="message">The input field cannot be empty</h3>`;
+        }
+        else {
         
-    fetch(url + userInput)
-        .then(resp => resp.json())
-        .then(data => {
-            let myDrink = data.drinks[0];
-            let count = 1;
-            let ingredients = [];
-            for (let i in myDrink) {
-                let ingredient = "";
-                let measure = "";
-                if (i.startsWith('strIngredient') && myDrink[i]) {
-                    ingredient = myDrink[i];
-                    if (myDrink[`strMeasure` + count]) {
-                        measure = myDrink[`strMeasure` + count];
+            fetch(url + userInput)
+                .then(resp => resp.json())
+                .then(data => {
+                    let myDrink = data.drinks[0];
+                    let count = 1;
+                    let ingredients = [];
+                    for (let i in myDrink) {
+                        let ingredient = "";
+                        let measure = "";
+                        if (i.startsWith('strIngredient') && myDrink[i]) {
+                            ingredient = myDrink[i];
+                            if (myDrink[`strMeasure` + count]) {
+                                measure = myDrink[`strMeasure` + count];
+                            }
+                            else {
+                                measure = "";
+                            }
+                            count += 1;
+                            ingredients.push(`${measure} ${ingredient}`);
+                        }
                     }
-                    else {
-                        measure = "";
-                    }
-                    count += 1;
-                    ingredients.push(`${measure} ${ingredient}`);
-                }
-            }
-            result.innerHTML = `
+                    result.innerHTML = `
             <img src = ${myDrink.strDrinkThumb}>
             <h2>${myDrink.strDrink}</h2>
             <h3>Ingredients:</h3>
@@ -45,21 +44,23 @@ document.addEventListener('DOMContentLoaded', (e) => {
             <p>${myDrink.strInstructions}</p>
 
             `;
-            let ingredientsContainer = document.querySelector('.ingredients');
+                    let ingredientsContainer = document.querySelector('.ingredients');
 
-            ingredients.forEach(item => {
-                let listItem = document.createElement('li');
-                listItem.textContent = item;
-                console.log(listItem)
-                ingredientsContainer.appendChild(listItem)
-            });
-        })
-        .catch(() => {
+                    ingredients.forEach(item => {
+                        let listItem = document.createElement('li');
+                        listItem.textContent = item;
+                        ingredientsContainer.appendChild(listItem)
+                    });
+                })
+                .catch(() => {
             
-            result.innerHTML = `<h3 class='message'>Please enter a valid input</h3>
+                    result.innerHTML = `<h3 class='message'>Please enter a valid input</h3>
 
             `
-        })
+                });
+        }
+
 }
 
-})
+window.addEventListener('load', getInfo)
+searchBtn.addEventListener('click', getInfo)
